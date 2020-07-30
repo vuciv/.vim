4f2844268fe500ec97f023a7370e0965f1fcb52a
Jersey Fonseca <jerseyfonseca@gmail.com>
2020-07-30 08:50:45 -0500

    Migrated to neovim on linux

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
-cp .vimrc ~/
+#!/bin/sh
+
+# Install vim-plug on linux
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Make dir if doesn't exist and add config
+mkdir -p ~/.config/nvim && cp init.vim ~/.config/nvim
+
 vim +PlugInstall +qa
 vim +PlugUpdate +qa
